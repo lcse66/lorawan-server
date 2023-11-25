39d09116e61fb2dc4f20d615cfaf4991810eded2
lcse66 <48930254+lcse66@users.noreply.github.com>
2023-11-26 00:05:09 +0100

    dev04

--- src/lorawan_connector_monitor.erl
+++ src/lorawan_connector_monitor.erl
@@ -50,7 +50,8 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-restart_connector(#connector{enabled=true, failed=[<<"network">> | <<"badarg">>]}=Connector) ->
+% restart_connector(#connector{enabled=true, failed=[<<"network">>]}=Connector) ->
+restart_connector(#connector{enabled=true) ->
     {atomic, ok} = mnesia:transaction(
         fun() ->
             lorawan_admin:write(Connector#connector{failed=[]})
